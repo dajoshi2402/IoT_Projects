779e0641f8ca5c1ef8a0c89dc9e1f446162fcbeb
dajoshi2402 <153915321+dajoshi2402@users.noreply.github.com>
2025-04-22 15:05:11 +0530

    Add files via upload
    Ambulance live GPS data and its mapping

---
+++ Ambulance_GPS_Final.ino
@@ -0,0 +1,122 @@
+//
+#include <SoftwareSerial.h>
+#include <String.h>
+#include <TinyGPS++.h>
+static const int RXPin = 3, TXPin = 4;
+static const uint32_t GPSBaud = 9600;
+ 
+SoftwareSerial mySerial(7, 8);
+TinyGPSPlus gps;
+
+boolean pin2=LOW,pin3=LOW,pin4=LOW,pin5=LOW,pin6=LOW; 
+float Lat=0.0;
+float Lan=0.0;
+
+SoftwareSerial ss(RXPin, TXPin);
+
+
+void setup()
+{
+  mySerial.begin(9600);               // the GPRS baud rate   
+  Serial.begin(9600);    // the GPRS baud rate 
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(4,INPUT);
+  pinMode(5,INPUT);  
+  pinMode(6,INPUT);  
+  delay(1000);
+  ss.begin(GPSBaud);
+}
+ 
+void loop()
+{
+       while (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= ");
+      Lat = gps.location.lat();
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6); 
+      Lan = gps.location.lng();    
+       Send2Pachube();
+    }
+       }
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+}
+void Send2Pachube()
+{
+  mySerial.println("AT");
+  delay(1000);
+
+  mySerial.println("AT+CPIN?");
+  delay(1000);
+
+  mySerial.println("AT+CREG?");
+  delay(1000);
+
+  mySerial.println("AT+CGATT?");
+  delay(1000);
+
+  mySerial.println("AT+CIPSHUT");
+  delay(1000);
+
+  mySerial.println("AT+CIPSTATUS");
+  delay(2000);
+
+  mySerial.println("AT+CIPMUX=0");
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CSTT=\"airtelgprs.com\"");//start task and setting the APN,
+  delay(1000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIICR");//bring up wireless connection
+  delay(3000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIFSR");//get local IP adress
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIPSPRT=0");
+  delay(3000);
+ 
+  ShowSerialData();
+  
+  mySerial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection
+  delay(6000);
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIPSEND");//begin send data to remote server
+  delay(4000);
+  ShowSerialData();
+  
+    //String str="GET http://api.thingspeak.com/update?api_key=FTEXM1E64X2CAU8D&field1=" + String(temp);
+    String str="GET http://api.thingspeak.com/update?api_key=OQJ18HUKEVIPAZ0N&field1=" + String(gps.location.lat()) + "&field2=" + String(gps.location.lng());
+  mySerial.println(str);//begin send data to remote server
+  delay(4000);
+  ShowSerialData();
+
+  mySerial.println((char)26);//sending
+  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
+  mySerial.println();
+ 
+  ShowSerialData();
+ 
+  mySerial.println("AT+CIPSHUT");//close the connection
+  delay(100);
+  ShowSerialData();
+} 
+void ShowSerialData()
+{
+  while(mySerial.available()!=0)
+    Serial.write(mySerial.read());
+}
